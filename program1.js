dc2e72b33084fd23b6422a9d41b380b49c7d4651
Gokul Pottumurti <123055226+gokulpottumurti80@users.noreply.github.com>
2024-11-11 14:32:03 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,10 +1,10 @@
-// const getTotalIsles = function (grid) {
+const getTotalIsles = function (grid) {
 
 
-//   // write your code here
+  // write your code here
 
-// };
+};
 
-// module.exports = getTotalIsles;
+module.exports = getTotalIsles;
 
 
