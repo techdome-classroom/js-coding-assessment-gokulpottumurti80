3bff8d0c8a9744519202cddc057d9df37a89c164
Gokul Pottumurti <123055226+gokulpottumurti80@users.noreply.github.com>
2024-11-11 14:37:20 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,40 @@
-const decodeTheRing = function (s, p) {
+function decodeTheRing(message, pattern) {
+  const memo = {};  // Memoization cache
 
-    // write your code here
+  function match(mIdx, pIdx) {
+      // Check if this sub-problem was already solved
+      const key = `${mIdx},${pIdx}`;
+      if (key in memo) return memo[key];
 
-  };
-  
-  module.exports = decodeTheRing;
+      // If both message and pattern are fully processed
+      if (mIdx === message.length && pIdx === pattern.length) return true;
+
+      // If pattern is used up but message isn't, or vice versa
+      if (pIdx === pattern.length) return false;
+
+      // Handle '*' in pattern
+      if (pattern[pIdx] === '*') {
+          // '*' matches 0 or more characters:
+          // Either move on in the pattern, or match it with a character in message
+          if (match(mIdx, pIdx + 1) || (mIdx < message.length && match(mIdx + 1, pIdx))) {
+              memo[key] = true;
+              return true;
+          }
+      }
+      // Handle '?' in pattern (matches exactly one character)
+      else if (pattern[pIdx] === '?' || (mIdx < message.length && pattern[pIdx] === message[mIdx])) {
+          if (match(mIdx + 1, pIdx + 1)) {
+              memo[key] = true;
+              return true;
+          }
+      }
+
+      // No match found
+      memo[key] = false;
+      return false;
+  }
+
+  return match(0, 0);  // Start recursion from the beginning of both strings
+}
+
+module.exports = decodeTheRing;
